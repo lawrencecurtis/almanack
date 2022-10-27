4556c255f181e56ed8ab60df7f0645cf853de5a2
Lawrence Curtis <lawrencecurtis@yellowpad.co.uk>
2022-10-27 14:14:15 +0100

    cleanup environment

--- lib/almanack/server/environment.rb
+++ lib/almanack/server/environment.rb
@@ -2,26 +2,27 @@ module Almanack
   module ServerContext
     module Environment
       def basename(file)
-        Pathname(file).split.last.to_s.split(".", 2).first
+        Pathname(file).split.last.to_s.split('.', 2).first
       end
 
       def locate_asset(name, within: path)
         name = basename(name)
         path = settings.root.join(within)
-        available = Pathname.glob(path.join("*"))
+        available = Pathname.glob(path.join('*'))
         asset = available.find { |p| basename(p) == name }
         raise "Could not find stylesheet #{name} inside #{available}" if asset.nil?
+
         asset
       end
 
       def auto_render_template(asset)
-        renderer = asset.extname.split(".").last
+        renderer = asset.extname.split('.').last
         content = asset.read
         respond_to?(renderer) ? send(renderer, content) : content
       end
 
       def auto_render_asset(*args)
-        auto_render_template locate_asset(args[0], within: args[1][:within])
+        auto_render_template locate_asset(args)
       end
 
       def theme_stylesheet_path
@@ -29,9 +30,7 @@ module Almanack
       end
 
       def register_sass_loadpaths!
-        if !SassC.load_paths.include?(theme_stylesheet_path)
-          SassC.load_paths << theme_stylesheet_path
-        end
+        SassC.load_paths << theme_stylesheet_path unless SassC.load_paths.include?(theme_stylesheet_path)
       end
     end
   end
